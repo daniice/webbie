35649f61bdf4d712f6d2f1989519d484babf42c2
daniice <daball@alumni.princeton.edu>
2023-04-23 11:30:39 -0400

    first commit

---
+++ js/imagesJs.js
@@ -0,0 +1,2 @@
+//drop down explanation of piece on hover
+//images resize with window - one or four at a time
---
+++ js/webbie.js
@@ -0,0 +1,41 @@
+//set background as a photo of paint swooshes
+//what to call third category of data/science/policy/climate
+//BALL engraved on ball - needs to be a solid border - Illustrator and export as svg
+//compatibility for ios?
+//github and push
+
+//dropdown menu on hover - three lines icon in img folder - disappears when not hovering 
+const dropDownBtn = document.querySelector('#dropDownBtn');
+const dropDown = document.querySelector('#dropDown');
+
+dropDownBtn.addEventListener('mouseover', () => {
+    dropDown.classList.add('show');
+})
+
+dropDown.addEventListener('mouseover', () => {
+    dropDown.classList.add('show');
+})
+
+dropDownBtn.addEventListener('mouseleave', () => {
+    dropDown.classList.toggle('show');
+})
+
+dropDown.addEventListener('mouseleave', () => {
+    dropDown.classList.toggle('show');
+})
+
+//ball animation
+const ball = document.querySelector('.ball');
+
+ball.addEventListener('mouseover', () => {
+    ball.classList.add('isHover');
+  });
+
+ball.addEventListener('mouseleave', () => {
+    ball.classList.toggle('isHover');
+})
+
+ball.addEventListener('click', () => {
+    ball.classList.remove('isHover');
+    ball.classList.add('onClick');
+  });
