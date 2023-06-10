f1c95189d8f172381d2cc60903a71f86dec819b1
daniice <daball@alumni.princeton.edu>
2023-06-10 09:05:39 -0400

    is checking successfully

--- js/webbie.js
+++ js/webbie.js
@@ -42,11 +42,11 @@
 
 
 //check for mobile for compatibility changes
-window.mobileCheck = function() {
-    let check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-    return check;
-  }
+let check = false;
+if(/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) check = true;
+
+if (check === true) console.log('mobile');
+if (check === false) console.log('noMobile');
 
 
 //dropdown menu on hover, lines flip up 90
@@ -54,12 +54,12 @@ const topBar = document.querySelector('#topBar');
 const dropDownBtn = document.querySelector('.topBarLine');
 const pageDrop = Array.from(document.getElementsByClassName('pageDrop'));
 
-if (window.mobileCheck === true) {
+if (check === true) {
+    dropDownBtn.classList.add('hide');
     pageDrop.forEach(page => {
         page.classList.add('show');
         page.classList.add('mobile');
     })
-    dropDownBtn.classList.add('hide');
 }
 
 else {
@@ -90,9 +90,9 @@ const ball = document.querySelector('.ball');
 const middleMain = document.querySelector('.middleMain');
 const welcomeBlurb = document.querySelector('.welcomeBlurb')
 
-if (window.mobileCheck === false) {
+if (check === false) {
     if (ball) {
-middleMain.addEventListener('mouseover', () => {
+    middleMain.addEventListener('mouseover', () => {
     ball.classList.add('isHover');
     welcomeBlurb.classList.add('show');
   });
@@ -106,8 +106,9 @@ middleMain.addEventListener('mouseleave', () => {
 
 
 //image page scroll set to none when mobile
-if (window.mobileCheck === true) {
-    const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector('.gallery');
+
+if (check === true) {
     if (gallery) gallery.classList.add('mobile')
 }
 
